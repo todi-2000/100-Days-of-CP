0afbf2ad0c381f2081328639034e87924201dcc7
Manshi Todi <manshitodi@gmail.com>
2021-01-24 23:46:29 +0530

    Practicr

---
+++ codeforces/61A.cpp
@@ -0,0 +1,44 @@
+// Question Link:
+#include<bits/stdc++.h>
+#include<cmath>
+#include<algorithm>
+#include<string.h>
+# define mod 1000000007
+using namespace std;
+typedef long long int lli;
+
+void solve()
+{
+    //write your code here
+    string a,b;
+    cin >> a >> b;
+    for(int i=0;i<a.size();i++)
+    {
+        if(a[i]==b[i])
+        cout << "0";
+        else
+        cout << "1";
+    }
+}
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen ("A:/c++/inputf.in", "r", stdin);
+    freopen ("A:/c++/outputf.in", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // int t;
+    // cin >> t;
+    // while(t--)
+    // {
+        solve();
+    // }
+    return 0;
+}
+
+
+
+
